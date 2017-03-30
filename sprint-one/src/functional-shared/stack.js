d9e7d3be2e0dc15ffc02b714f092ce47235b45fb
Hack Reactor Student <student@hackreactor.com>
2017-03-29 18:29:50 -0700

    Passed all tests in Functional Pattern and Stack on Shared Function

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
+	var store = {};
+	store.count = 0
+	extend(store, stackMethods);
+	return store;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var count = 0;
 
 
+stackMethods = {};
+
+stackMethods.size = function () {
+	return this.count;
+}
+
+stackMethods.push = function(value) {
+  this[this.count] = value;
+  this.count++;
+}
+
+stackMethods.pop = function() {
+	if (this.count > 0) {
+		this.count--;
+	return this[this.count];
+	}
+}
