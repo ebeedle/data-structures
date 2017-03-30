16292bcba247cf2f3144d2b5ad0688534cf23942
Hack Reactor Student <student@hackreactor.com>
2017-03-30 16:28:53 -0700

    Passed all tests, however did not use Node constructor

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,37 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = {};
+  list.tail = {};
+  var length = 0;
+  var addedPlaceHolder = 0;
+  var removedPlaceHolder = 0;
 
   list.addToTail = function(value) {
-  };
+    list.tail.value = value;
+    list[addedPlaceHolder] = value;
+      if (length === 0) {
+        list.head.value = value;
+      }
+    addedPlaceHolder++;
+    length++;
+    };
 
   list.removeHead = function() {
+    removedPlaceHolder++;
+    length--;
+    delete list[removedPlaceHolder - 1]
+    var head = list.head.value;
+    list.head.value = list[removedPlaceHolder];
+    return head;
   };
 
   list.contains = function(target) {
+    for (var key in list) {
+      if (list[key] === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +48,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   O(n) (Linear) because of for-in loop
  */
