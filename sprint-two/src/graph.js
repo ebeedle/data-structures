867f545d999a58fb43a04d676eb9f453f8130164
Hack Reactor Student <student@hackreactor.com>
2017-03-31 18:38:09 -0700

    Passed first 4 tets

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -6,22 +6,35 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = {'node': node};
+
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this[node]) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this[toNode].fromNode === fromNode && this[fromNode].toNode === toNode) {
+    return true;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode].toNode = toNode;
+  this[toNode].fromNode = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
